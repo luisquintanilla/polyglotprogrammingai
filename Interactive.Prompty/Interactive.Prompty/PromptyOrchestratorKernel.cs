e4e809ff6ee3698824130868aa6b852ab8544fef
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-10-09 23:47:05 -0700

    update notebook

--- Interactive.Prompty/Interactive.Prompty/PromptyOrchestratorKernel.cs
+++ Interactive.Prompty/Interactive.Prompty/PromptyOrchestratorKernel.cs
@@ -1,10 +1,10 @@
-﻿using System.Text;
-using Microsoft.DotNet.Interactive;
+﻿using Microsoft.DotNet.Interactive;
 using Microsoft.DotNet.Interactive.Commands;
-using Microsoft.DotNet.Interactive.CSharp;
 using Microsoft.DotNet.Interactive.Events;
 using Microsoft.DotNet.Interactive.Formatting;
+using Microsoft.DotNet.Interactive.ValueSharing;
 using Microsoft.SemanticKernel;
+using System.Text;
 using Kernel = Microsoft.DotNet.Interactive.Kernel;
 using SKernel = Microsoft.SemanticKernel.Kernel;
 #pragma warning disable SKEXP0040
@@ -13,8 +13,8 @@ namespace Interactive.Prompty;
 
 public class PromptyOrchestratorKernel : Kernel,
     IKernelCommandHandler<SendValue>,
-
-IKernelCommandHandler<SubmitCode>
+    IKernelCommandHandler<RequestValueInfos>,
+    IKernelCommandHandler<SubmitCode>
 {
     private readonly string _promptyKernel;
     private string _promptyCode = string.Empty;
@@ -70,17 +70,29 @@ IKernelCommandHandler<SubmitCode>
 
         StringBuilder fullContent = new();
         var displayThing = context.Display(fullContent.ToString(), [PlainTextFormatter.MimeType]);
-        await foreach (var content  in _kernelFunction.InvokeStreamingAsync(_kernel, args, context.CancellationToken))
+        await foreach (var content in _kernelFunction.InvokeStreamingAsync(_kernel, args, context.CancellationToken))
         {
             fullContent.Append(content);
             displayThing.Update(fullContent.ToString());
-        }                    
+        }
 
     }
 
     Task IKernelCommandHandler<SendValue>.HandleAsync(SendValue command, KernelInvocationContext context)
     {
         _values[command.Name] = command.FormattedValue.Value;
+        return Task.CompletedTask;
+    }
+
+    public Task HandleAsync(RequestValueInfos command, KernelInvocationContext context)
+    {
+        var values = _values.Select(x => new KernelValueInfo(x.Key, new FormattedValue(PlainTextFormatter.MimeType, x.Value?.ToString() ?? string.Empty)));
+        var valueInfosProduced = new ValueInfosProduced(
+            command: command,
+            valueInfos: values.ToArray());
+
+        context.Publish(valueInfosProduced);
+
         return Task.CompletedTask;
     }
 }
